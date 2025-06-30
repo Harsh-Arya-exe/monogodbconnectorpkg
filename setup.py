fc69c56bbe812ff64c190da12c6ef9da89b88418
Harsh-Arya-exe <harsh.arya1004@gmail.com>
2025-06-30 21:43:26 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HIPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path : str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = [requirements.replace() for requirements in f.readlines()] 
+
+        if "HIPHEN_E_DOT" in requirements:
+            requirements.remove("HIPHEN_E_DOT")
+    return requirements
+
+
+__version__ = "0.0.1"
+REPO_NAME = "monogodbconnectorpkg"
+PKG_NAME = "automationdatabase"
+AUTHOR_USER_NAME = "harsharya" 
+AUTHOR_EMAIL = "harsh.arya1004@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database."
+    long_description=,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+)
---
+++ template.py
@@ -0,0 +1,35 @@
+import os 
+from pathlib import Path
+
+package_name = "mongodb_connect"
+
+list_of_files = [
+    ".github/workflows/ci.yaml",
+    "src/__init__.py",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/mongo_crud.py",
+    "testing/_init__/py",
+    "testing/unit/__init__.py",
+    "testing/unit/unit.py",
+    "testing/integration/__init__.py",
+    "testing/integration/int.py",
+    "experiment/experiments.ipynb",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "init_setup.sh",
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "tox.ini"
+]
+
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    dir_name, file_name = os.path.split(file_path)
+    if dir_name != "":
+        os.makedirs(dir_name, exist_ok=True)
+    
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        with open(file_path, 'w') as f:
+            pass
